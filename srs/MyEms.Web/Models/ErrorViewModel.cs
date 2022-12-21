10f5c211d8ee744df74f5d37234fc6bef4c7c512
Ishan Pandya <ishanpandya96@gmail.com>
2022-12-21 15:00:25 -0500

    Added Web and BL Class

---
+++ srs/MyEms.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace MyEms.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
